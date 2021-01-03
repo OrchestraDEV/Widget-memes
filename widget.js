61b735c33aba403ff684a6551cb71c70b5cb3274
OrchestraDEV <74406786+OrchestraDEV@users.noreply.github.com>
2021-01-03 12:55:51 +0600

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,10 +1,10 @@
 const app = require('widget');
 
-// geting manifest data with array of meme links
+// get manifest data with array of meme links
 const manifest = app.getManifest();
 
 setTimeout(() => {
-	const pics = manifest.data_sources.pictures;
+	const pics = manifest.data_sources.pictures; // array of pictures links here
 	const ind = Math.floor(Math.random() * pics.length); // generating random number to pick random meme
-	app.publish(`<img src="${pics[ind - 1]}">`);
+	app.publish(`<img src="${pics[ind - 1]}">`); // generating HTML chunk with single img tag and sending result to user
 }, 5*1000)
